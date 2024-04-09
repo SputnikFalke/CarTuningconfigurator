32b11a69afea0331fc358659cea6eeb0fe081d93
Fabio <fabio.dauru@bwz-rappi.ch>
2024-04-09 08:43:03 +0200

    commit1

---
+++ CarTuningConfigurator/DatabaseConnection/DBConnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace CarTuningConfigurator.DatabaseConnection
+{
+    class DBConnect
+    {
+        const string DbConnectionString = "";
+    } 
+}
