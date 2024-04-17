d6e3de76469aa0648c38d56d5a070d6aac04055d
Fabio <fabio.dauru@bwz-rappi.ch>
2024-04-17 15:18:33 +0200

    kleinigkeit

--- CarTuningConfigurator/Contorller/HomePageController.cs
+++ CarTuningConfigurator/Contorller/HomePageController.cs
@@ -28,42 +28,7 @@ namespace CarTuningConfigurator.Contorller
 
         public void updateTunningPartfromCar(Car car, List<TunningPart> tunningParts)
         {
-
             car.tunningParts = tunningParts;
-            int horsepower = car.Horsepower;
-            double brakeforce = car.Brakeforce;
-            double traction = car.Traction;
-            double weight = car.Weight;
-            int highspeed = car.Highspeed;
-            double price = car.Price;
-            double acceleration = car.Acceleration;
-            CarModel = new CarModel();
-            CarModel.updateTunningPartsFromCar(car.Model, tunningParts);
-
-            string img = car.Image.ToString();
-            string lilImg = car.LittleImage.ToString();
-
-            foreach(var tunningPart in tunningParts)
-            {
-                horsepower = horsepower + tunningPart.ChangeOfHorsePower;
-                brakeforce = brakeforce + tunningPart.ChangeOfBrakeForce;
-                traction = traction + tunningPart.ChangeOfTraction;
-                weight = weight + tunningPart.ChangeOfWeight;
-                highspeed = highspeed + tunningPart.ChangeOfHighspeed;
-                price = price + tunningPart.ChangeOfPrice;
-                acceleration = acceleration + tunningPart.ChangeOfAcceleration;
-            }
-            if(brakeforce > 10)
-            {
-                brakeforce = 10;
-            }
-            if(traction > 10)
-            {
-                traction = 10;
-            }
-            Car car1 = new Car(car.Model, car.Brand, img, lilImg, horsepower, brakeforce, traction, weight, highspeed, acceleration, price, true, tunningParts);
-            return car1;
-
         }
 
         public void saveCar(User user, Car car)
